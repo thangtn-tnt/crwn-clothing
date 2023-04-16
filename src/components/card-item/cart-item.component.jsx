9115dfcdf9a078af4894afd3ec856983c841da7e
Trương Nhut Thăng <nhutthang.t@gmail.com>
2023-04-16 19:14:19 +0700

    checkout page with increment, decrement, remove item and total price from cart

--- src/components/card-item/cart-item.component.jsx
+++ src/components/card-item/cart-item.component.jsx
@@ -9,7 +9,7 @@ export default function CartItem({ cartItem }) {
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
-          {quantity} x ${price}
+          {quantity} x {price}
         </span>
       </div>
     </div>
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,11 +3,18 @@ import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
 import CartItem from "../card-item/cart-item.component";
 
+import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 
 export default function CartDropdown() {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -15,7 +22,9 @@ export default function CartDropdown() {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button buttonType="inverted">GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler} buttonType="inverted">
+        GO TO CHECKOUT
+      </Button>
     </div>
   );
 }
